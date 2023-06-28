b6dc7258f190e8d1f353e3184cbbab57efa48cb4
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2023-06-28 20:52:32 +0300

    loginpage style

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react'
-import { useActions } from '../hooks/useActions';
+import { useActions } from '../hooks/useActions'
+import './UI/button/ButtonStyle.css';
 
 
 const LoginForm: FC = () => {
@@ -11,31 +12,42 @@ const LoginForm: FC = () => {
     const [password, setPassword] = useState<string>('');
 
     return (
-        <div>
-            <input
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                type='text'
-                placeholder='Email' />
+        <main id='app-container'>
+            <section id='page'>
+                <div className='container'>
+                    <div className='container__wrapper'>
+                        <div className='input-container'>
+                            <input className='input__input'
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                type='text'
+                                placeholder='Email' />
+                        </div>
+                        <div className='input-container'>
+                            <input className='input__input'
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                type='password'
+                                placeholder='Password' />
+                        </div>
 
-            <input
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                type='password'
-                placeholder='Password' />
+                        <button className='btn btn-orange' onClick={() => login(email, password)}>
+                            Войти
+                        </button>
 
-            <button onClick={() => login(email, password)}>
-                Войти
-            </button>
+                        <button className='btn' onClick={() => signup(email, password)} >
+                            Зарегистрироваться
+                        </button>
 
-            <button onClick={() => signup(email, password)} >
-                Зарегистрироваться
-            </button>
+                        <button className='btn' onClick={() => logout()}>
+                            Выйти
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </main>
 
-            <button onClick={() => logout()}>
-                Выйти
-            </button>
-        </div>
+
     )
 }
 
